b0186018a2672ecf9570b9d592930c8d8b2d6956
erospereira <per18044@byui.edu>
2019-02-20 19:25:17 -0700

    lesson7

---
+++ lesson-7/scripts/windchill.js
@@ -0,0 +1,7 @@
+let x = parseInt(document.getElementById('temperature').innerHTML);
+
+let y = parseInt(document.getElementById('windChill').innerHTML);
+
+let result = 35.74 + 0.6215 * x - 35.75 * y ^ 0.16 + .4275 * x * y ^ 0.16;
+
+document.getElementById('output').innerHTML = result.toFixed(1);
