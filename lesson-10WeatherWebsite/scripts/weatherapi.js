ab6fffc7e8636510ca2ddd907ae6747c201e1a8e
erospereira <per18044@byui.edu>
2019-03-17 15:46:20 -0400

    jj

--- lesson-10WeatherWebsite/scripts/weatherapi.js
+++ lesson-10WeatherWebsite/scripts/weatherapi.js
@@ -2,7 +2,7 @@
 let weatherRequest = new XMLHttpRequest();
 
 
-weatherRequest.open("GET","http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=588f3b31c6fe7beb50679f8681db46fc", true);
+weatherRequest.open("GET","https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=588f3b31c6fe7beb50679f8681db46fc", true);
 weatherRequest.send();
 
 weatherRequest.onload =  function () {
